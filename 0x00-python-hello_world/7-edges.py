517b602d96b007bc31e6dda20c82aa78ef2fb745
samutrujillo <samueltrujillorodriguez@gmail.com>
2021-04-26 18:35:47 -0500

    [<update ~ tools](<Python - Hello, World>)

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-print("First 3 letters: {}".format(word[:3]))
-print("Last 2 letters: {}".format(word[7:]))
-print("Middle word: {}".format(word[1:8]))
+word_first_3 = word[:3]
+word_last_2 = word[7:]
+middle_word = word[1:8]
+print("First 3 letters: {}".format(word_first_3))
+print("Last 2 letters: {}".format(word_last_2))
+print("Middle word: {}".format(middle_word))
