284a480da6438d5ee7a1c7feda21dc3d538c389f
samutrujillo <samueltrujillorodriguez@gmail.com>
2021-04-28 17:28:50 -0500

    [<update ~ 0-add..py](<python-import_modules>)

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-add = __import__('add_0').add
-a = 1
-b = 2
-result = add(a, b)
-print("{} + {} = {}".format(a, b, result))
+from add_0 import add
+if __name__ == '__main__':
+    a = 1
+    b = 2
+    result = add(a, b)
+    print("{} + {} = {}".format(a, b, result))
