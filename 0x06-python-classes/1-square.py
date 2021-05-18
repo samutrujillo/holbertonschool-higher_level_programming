365f865705e39b35e314942e221d3742e7a11be4
samutrujillo <samueltrujillorodriguez@gmail.com>
2021-05-18 17:49:37 -0500

    [<0x06 ~ 1-square.py](<Python - Classes and Objects>)

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+
+class Square that defines a square
+"""
+
+
+class Square:
+    """empty class"""
+    def __init__(self, size):
+        """constructor"""
+        self.__size = size
