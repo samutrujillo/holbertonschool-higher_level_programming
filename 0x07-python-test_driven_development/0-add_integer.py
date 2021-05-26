e298a3ca03673eb1e99c681bf3f184921c6f8d9a
samutrujillo <samueltrujillorodriguez@gmail.com>
2021-05-26 00:07:04 -0500

    [<0x07 ~ update file](<Python - Test-driven development>)

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -4,12 +4,15 @@
 
 def add_integer(a, b=98):
     """finction"""
-    if type(a) is float:
-        a = int(a)
-    if type(b) is float:
-        b = int(b)
-    if type(a) is not int:
-        raise TypeError("a must be an integer")
-    if type(b) is not int:
-        raise TypeError("b must be an integer")
-    return a + b
+    try:
+        if type(a) is float:
+            a = int(a)
+        if type(b) is float:
+            b = int(b)
+        if type(a) is not int:
+            raise TypeError("a must be an integer")
+        if type(b) is not int:
+            raise TypeError("b must be an integer")
+        return a + b
+    except:
+        raise
