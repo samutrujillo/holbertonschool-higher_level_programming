cd4f475fe15c23ab38c679fdf3c6fe9a763934fa
samutrujillo <samueltrujillorodriguez@gmail.com>
2021-05-26 00:16:49 -0500

    [<0x07 ~ update file](<Python - Test-driven development>)

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -4,27 +4,25 @@
 
 def matrix_divided(matrix, div):
     """ function """
-    try:
-        if not isinstance(div, (float, int)) or isinstance(div, bool):
-            raise TypeError("div must be a number")
-        if div == 0:
-            raise ZeroDivisionError("division by zero")
-        if not all(len(lts) == len(matrix[0]) for lts in matrix):
+    if type(matrix) is not list:
+        raise TypeError("matrix must be a matrix " +
+                        "(list of lists) of integers/floats")
+    sub_l_len = 0
+    for sub_l in matrix:
+        for i in sub_l:
+            if type(i) not in [int, float]:
+                raise TypeError("matrix must be a matrix " +
+                                "(list of lists) of integers/floats")
+
+        if sub_l_len == 0:
+            sub_l_len = len(sub_l)
+        elif sub_l_len != len(sub_l):
             raise TypeError("Each row of the matrix must have the same size")
 
-        if not isinstance(matrix, list) or \
-                len(matrix) == 0 or len(matrix[0]) == 0:
-            raise TypeError("matrix must be a matrix (list of lists)"
-                            " of integers/floats")
-        for a in matrix:
-            if not isinstance(a, list) or not len(a) > 0 or not \
-                 all(isinstance(column, (int, float)) for column in a):
-                raise TypeError("matrix must be a matrix (list of lists)"
-                                " of integers/floats")
-        matrix_2 = []
-        for i in matrix:
-            matrix_2.append(list(map(lambda new_num:
-                            round(new_num / div, 2), i)))
-        return matrix_2
-    except:
-        raise
+    if type(div) not in [int, float]:
+        raise TypeError("div must be a number")
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    return [[round(i / div, 2) for i in sub_l] for sub_l in matrix]
