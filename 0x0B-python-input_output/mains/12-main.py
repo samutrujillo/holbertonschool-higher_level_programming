acfbf9c94dad88df5158ac1f135f67071cf20b03
samutrujillo <samueltrujillorodriguez@gmail.com>
2021-05-31 21:11:59 -0500

    [<0x0B ~ 12-pascal_triangle.py](<Python - Input/Output>)

---
+++ 0x0B-python-input_output/mains/12-main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import sys
+"""
+12-main
+"""
+
+
+if __name__ == "__main__":
+    sys.path.append("..")
+    pascal_triangle = __import__('12-pascal_triangle').pascal_triangle
+
+def print_triangle(triangle):
+    """
+    Print the triangle
+    """
+    for row in triangle:
+        print("[{}]".format(",".join([str(x) for x in row])))
+
+
+if __name__ == "__main__":
+    print_triangle(pascal_triangle(5))
