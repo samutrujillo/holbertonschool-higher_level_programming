14122bb3245047071295e34b86443b8737b2ced7
samutrujillo <samueltrujillorodriguez@gmail.com>
2021-05-31 20:09:10 -0500

    [<0x0B ~ 6-load_from_json_file.py](<Python - Input/Output>)

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" function that creates an Object from a “JSON file” """
+import json
+
+
+def load_from_json_file(filename):
+    """ function """
+    with open(filename, encoding='utf-8') as fichero:
+        return json.loads(fichero.read())
