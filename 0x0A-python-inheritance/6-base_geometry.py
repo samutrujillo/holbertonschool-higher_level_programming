17b2256de01f13a19ea3c99bc3147a10ea3bfc22
samutrujillo <samueltrujillorodriguez@gmail.com>
2021-06-02 19:59:59 -0500

    [<0x0A ~ 6-base_geometry.py](<Python - Inheritance>)

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" class BaseGeometry """
+
+
+class BaseGeometry:
+    """ constructor """
+    def area(self):
+        """ function """
+        raise Exception('area() is not implemented')
